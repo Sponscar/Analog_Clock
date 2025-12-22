7dd3746378cdec065ab34a9bd8e39b4a26c55a59
Sponscar <panji.sabilillah23@student.uisi.ac.id>
2025-12-22 14:44:18 +0700

    Menambahkan digital clock

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
 const hr = document.querySelector("#hr");
 const mn = document.querySelector("#mn");
 const sc = document.querySelector("#sc");
+const digitalClock = document.querySelector("#digitalClock");
 
 const deg = 6;
 
 setInterval(() => {
-	let day = new Date();
-	let hh = day.getHours() * 30;
-	let mm = day.getMinutes() * deg;
-	let ss = day.getSeconds() * deg;
-	console.log(hh);
-	console.log(mm);
-	console.log(ss);
-	hr.style.transform = `rotate(${hh+(mm/12)}deg)`;
-	mn.style.transform = `rotate(${mm}deg)`;
-	sc.style.transform = `rotate(${ss}deg)`;
-});
+  let day = new Date();
+
+  let hours = day.getHours();
+  let minutes = day.getMinutes();
+  let seconds = day.getSeconds();
+
+  // Analog clock
+  let hh = hours * 30;
+  let mm = minutes * deg;
+  let ss = seconds * deg;
+
+  hr.style.transform = `rotate(${hh + mm / 12}deg)`;
+  mn.style.transform = `rotate(${mm}deg)`;
+  sc.style.transform = `rotate(${ss}deg)`;
+
+  // Digital clock
+  const pad = (n) => n.toString().padStart(2, '0');
+  digitalClock.textContent = `${pad(hours)}:${pad(minutes)}:${pad(seconds)}`;
+}, 1000);
